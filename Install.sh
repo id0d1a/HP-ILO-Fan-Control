e08b606f7bf7ff501f7c5105ab34c01695fc8409
Thomas Wilbur <thomaswilbur@adaclare.com>
2021-11-16 14:24:02 -0600

    Modify for my own use

--- Install.sh
+++ Install.sh
@@ -10,13 +10,13 @@ if [ $input = yes ] ; then
         apt install sshpass wget lm-sensors jq -y
         echo "Installed required apt packages"
         cd /
-        echo "Downloading ILO_250 for ROM Upgrade"
-        echo "Downloading to / directory"
-        wget https://github.com/That-Guy-Jack/HP-ILO-Fan-Control/tree/main/Files/ilo_250
-        echo "Finished Download"
+        #echo "Downloading ILO_250 for ROM Upgrade"
+        #echo "Downloading to / directory"
+        #wget https://github.com/That-Guy-Jack/HP-ILO-Fan-Control/tree/main/Files/ilo_250
+        #echo "Finished Download"
         echo "Making autofan.service"
         cd /etc/systemd/system/
-        wget https://raw.githubusercontent.com/That-Guy-Jack/HP-ILO-Fan-Control/main/Files/autofan.service
+        wget https://raw.githubusercontent.com/thomaswilbur/HP-ILO-Fan-Control/main/Files/autofan.service
         echo "Finished making autofan.service"
         echo "Which server are you running? Dl360p 1u (1) or the DL380p 2u (2) [1] or [2] :"
                 read server
@@ -24,21 +24,17 @@ if [ $input = yes ] ; then
                         echo "Preping autofan.sh for DL360p "
                         cd /
                         echo "Downloading Latest autofan.sh"
-                        wget https://raw.githubusercontent.com/That-Guy-Jack/HP-ILO-Fan-Control/main/Files/autofan.sh
+                        wget https://raw.githubusercontent.com/thomaswilbur/HP-ILO-Fan-Control/main/Files/autofan.sh
                         echo "Downloaded autofan.sh Change Placeholders with correct info"
-                        echo "Please Visit the Github Page to Follow Instructions"
-                        echo "https://github.com/That-Guy-Jack/HP-ILO-Fan-Control"
                         exit 1
                 elif [ $server = 2 ] ; then
                         echo "Preping autofan.sh for DL380p "
                         cd /
                         echo "Downloading Latest autofan.sh"
-                        wget https://raw.githubusercontent.com/That-Guy-Jack/HP-ILO-Fan-Control/main/Files/autofan-dl380p-g8.sh
+                        wget https://raw.githubusercontent.com/thomaswilbur/HP-ILO-Fan-Control/main/Files/autofan-dl380p-g8.sh
                         echo "Renaming File"
                         mv autofan-dl380p-g8.sh autofan.sh
                         echo "Downloaded autofan.sh Change Placeholders with correct info"
-                        echo "Please Visit the Github Page to Follow Instructions"
-                        echo "https://github.com/That-Guy-Jack/HP-ILO-Fan-Control"
                         exit 1
                 fi
 else
